2b4d98be0d88f98e8785f95be45d5deb9c148c82
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-02-17 16:44:58 +0300

    Add files via upload

---
+++ RecyclerViewOrnek/app/src/main/java/com/kis/recyclerviewornek/RecyclerViewAdapter.java
@@ -0,0 +1,11 @@
+package com.kis.recyclerviewornek;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.SosyalMedyaAlani> {
+
+    class SosyalMedyaAlani{
+
+
+    }
+}
